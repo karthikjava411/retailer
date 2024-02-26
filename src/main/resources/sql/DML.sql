e5fbd7670afbcd5185c7cccdd996d430ba082102
karthikjava411 <160618885+karthikjava411@users.noreply.github.com>
2024-02-26 06:20:02 -0600

    Added sql file

---
+++ src/main/resources/sql/DML.sql
@@ -0,0 +1,31 @@
+INSERT INTO CUSTOMER
+    ( Customer_Name, Phone, Email, Address1, Address2, City, State, ZipCode, CreatedBy, CreatedDate) 
+    VALUES 
+    ('Mike Bond', 9012226666, 'mike.bond@retailer.com', '100 Lake Dr', '', 'St Louis', 'MO', '63132','mike', CURRENT_TIMESTAMP)
+    
+    INSERT INTO ItemDetails
+    ( Item_Name, Item_Price, CreatedBy, CreatedDate) 
+    VALUES 
+    ('Pendrive', 10, 'sathya', CURRENT_TIMESTAMP);
+   
+
+    INSERT INTO ItemDetails
+    ( Item_Name, Item_Price, CreatedBy, CreatedDate) 
+    VALUES 
+    ('Novel', 50, 'sathya', CURRENT_TIMESTAMP);
+    
+
+    INSERT INTO ItemDetails
+    ( Item_Name, Item_Price, CreatedBy, CreatedDate) 
+    VALUES 
+    ('Notebook', 50, 'sathya', CURRENT_TIMESTAMP);
+
+    INSERT INTO ItemDetails
+    ( Item_Name, Item_Price, CreatedBy, CreatedDate) 
+    VALUES 
+    ('Phone', 500, 'sathya', CURRENT_TIMESTAMP);
+
+    INSERT INTO ItemDetails
+    ( Item_Name, Item_Price, CreatedBy, CreatedDate) 
+    VALUES 
+    ('Charger', 50, 'sathya', CURRENT_TIMESTAMP);
